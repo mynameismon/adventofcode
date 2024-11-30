33d1aa40eaf92a8b9960c18ae6c23784b54b5103
mynameismon <42601474+mynameismon@users.noreply.github.com>
2024-12-01 01:21:59 +0530

    2023 rust: add day 2

--- 2023/rust/src/main.rs
+++ 2023/rust/src/main.rs
@@ -1,18 +1,12 @@
 pub mod day_1;
+pub mod day_2;
 pub mod utils;
 
-// use std::io;
-
 fn main() {
-    // println!("Enter a day to run: ");
-
-    // let mut day_str = String::new();
-    // io::stdin()
-    //     .read_line(&mut day_str)
-    //     .expect("Unable to read day!");
-
-    // let day: u32 = day_str.trim().parse().expect("Not an integer!");
 
     println!("Running day 1");
     day_1::solve();
+
+    println!("Running day 2");
+    day_2::solve();
 }
