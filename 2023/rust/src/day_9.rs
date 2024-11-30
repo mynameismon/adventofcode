9abbf996ab13a90a0214016445617f7509c222ed
mynameismon <42601474+mynameismon@users.noreply.github.com>
2024-12-01 01:21:59 +0530

    2023 rust: finish day 9 (easy stuff)

---
+++ 2023/rust/src/day_9.rs
@@ -0,0 +1,41 @@
+use crate::utils::read_lines;
+
+fn find_extrapolation (nums: &Vec<i32>) -> i32 {
+    let nums_shifted = &nums[1..];
+    let nums_rest = nums.split_last().unwrap().1;
+    let diff_array : Vec<i32> = nums_shifted.iter()
+                                            .zip(nums_rest)
+                                            .map(|(x, y)| x - y)
+                                            .collect();
+    let zeroes: Vec<i32> = diff_array.clone().into_iter().filter(|x| *x != 0).collect();
+    if zeroes.is_empty() {
+        nums.last().unwrap() + 0
+    }
+    else {
+        let rec_call: i32 = find_extrapolation(&diff_array);
+        nums.last().unwrap() + rec_call
+    }
+}
+
+fn part_1 () -> i32{
+    let lines: Vec<String> = read_lines("../inputs/d9.txt");
+
+    lines.into_iter()
+         .map(|x| x.split_whitespace().map(|y| y.parse::<i32>().unwrap()).collect())
+         .map(|x| find_extrapolation(&x))
+         .sum::<i32>()
+}
+
+fn part_2 () -> i32 {
+    let lines: Vec<String> = read_lines("../inputs/d9.txt");
+
+    lines.into_iter()
+         .map(|x| x.split_whitespace().map(|y| y.parse::<i32>().unwrap()).rev().collect())
+         .map(|x| find_extrapolation(&x))
+         .sum::<i32>()
+}
+
+pub fn solve () {
+    println!("Answer to part 1: {}", part_1());
+    println!("Answer to part 2: {}", part_2());
+}
--- 2023/rust/src/main.rs
+++ 2023/rust/src/main.rs
@@ -1,5 +1,6 @@
 pub mod day_1;
 pub mod day_2;
+pub mod day_9;
 pub mod utils;
 
 fn main() {
@@ -9,4 +10,8 @@ fn main() {
 
     println!("Running day 2");
     day_2::solve();
+
+    // TODO: All the way from day 2 to 9
+    println!("Running day 9");
+    day_9::solve();
 }
